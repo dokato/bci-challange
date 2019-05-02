c4d980cd675e596bab34a66b45fd1c0d4956a1c0
Dominik Krzeminski (dokato) <raymon92@gmail.com>
2019-05-02 09:57:04 +0100

    fixes to smoother

--- sketch4.m
+++ sketch4.m
@@ -1,12 +1,12 @@
 % Attempt to do p300 prediction with Riemannian geometry approach
-addpath('utils/')
+%addpath('utils/')
 % add covariance toolbox to path as well
-clear all
+%clear all
 
 % data reading
 %%%%%%%%%%%%%%%%
-subj = '10';
-session = '01';
+%subj = '10';
+%session = '01';
 data_load;
 %%%%%%%%%%%%%%%%
 
@@ -33,13 +33,19 @@ COVval = covariances(Xval);
 COV_gf = geodesic_filter(COV,Cg,W(:,1:5-1));
 COVval_gf = geodesic_filter(COVval,Cg,W(:,1:5-1));    
 
+% standard mdm procedure
+[Ypred, d, C] = mdm(COVval_gf,COV_gf,Ytr,'riemann','riemann');
+Yps = smooth_prediction(d);
+Acc = mean(Yps==Yval);
+
+% ensemble of riemanniam features
 riem_feat = flatten_spd(COV_gf)';
 riem_feat_val = flatten_spd(COVval_gf)';
 
 cfg = [];
 cfg.strategy = 'dval';
-cfg.nsamples = 0.5;
-cfg.nfeatures = 0.2;
+cfg.nsamples = 0.4;
+cfg.nfeatures = 0.8;
 cfg.nlearners = 500;
 cfg.stratify = 1;
 cfg.bootstrap = 1;
@@ -50,11 +56,5 @@ ensemble_clf = train_ensemble(cfg,riem_feat,Ytr+1);
 
 [Ypred, d] = test_ensemble(ensemble_clf, riem_feat_val);
 Ypred = Ypred - 1;
-
-%[Ypred, d, C] = mdm(COVval_gf,COV_gf,Ytr,'riemann','riemann');
-
-%Yps = smooth_prediction(d);
-
-mean(Ypred==Yval)
-
-mean(Yps==Yval)
+Yps = smooth_prediction(-d); % minus, because negative denotes p300 here
+Acc_ens = mean(Yps==Yval);
--- utils/smooth_prediction.m
+++ utils/smooth_prediction.m
@@ -1,8 +1,9 @@
 function [Ypred] = smooth_prediction(d)
 %SMOOTH_PREDICTION Summary of this function goes here
 % INPUT:
-%    d - matrix of distances to classes. Column  1 - distance to p300
-%        column 2 - not target (trials, 2 ). 
+%    d - matrix of distances to classes. Can be of a shape: 
+%        (trials, 2 ) - Column  1 - distance to p300, Column 2 - not target  or  
+%        (trials, 1 ) - negative distance to 1, positive to 0
 % OUTPUT:
 %    Ypred - smooth Ypred vector (trials,1)
 
@@ -12,7 +13,11 @@ end
 
 Ypred = zeros(size(d,1),1);
 
-dd = d(:,1)-d(:,2);
+if size(d,2) == 2
+    dd = d(:,1)-d(:,2);
+else
+    dd = d;
+end
 
 for ii=1:(size(d,1)/8)
     run_indices = ((ii - 1)*8+1): (ii*8);
